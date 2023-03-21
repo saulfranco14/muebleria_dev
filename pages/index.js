4279a0c7fff65acb89a76a293b1c25d6038bc77e
saulfranco14 <saul.franco1420@gmail.com>
2023-03-21 17:35:54 -0600

    Update page into folder pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../src/app/page.module.css';
+
+export default function Index() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Muebleria Plus</h1>
+      </div>
+    </main>
+  )
+}
